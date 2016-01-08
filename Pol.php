62e2f98d39cf32434a8418af600f828be1e609e3
Tarkik Romanski <tarkik@ukr.net>
2016-01-08 10:31:53 +0200

    відредактований файл опису + добавлена елементарна структура + іконка +gitignore file

--- Pol.php
+++ Pol.php
@@ -1,3 +1,13 @@
 <?php
 namespace Pol;
 
+class isType {
+    public function isNumeric($a) {
+
+    }
+
+    public function isFunction($a) {
+        $a = strip_tags(trim($a));
+
+    }
+}
--- index.php
+++ index.php
@@ -7,19 +7,44 @@ header('Charset:UTF-8');
 <head>
     <meta charset="utf-8">
     <title>LUI | Кишеньковий математик</title>
-    <link rel="stylesheet" href="style/main.css">
     <link rel="shortcut icon" href="image/lui%20logo16.png" type="image/png">
+    <link rel="stylesheet" href="style/main.css">
+    <script type="application/javascript" src="js/main.js"></script>
 </head>
 <body>
 <h1>Lui</h1>
 <h2>Ваш персональний математик у вас в кишені</h2>
-<form>
-    <p>Введіть вираз: </p>
-    <input type="text" name="formula" id="formula">
-    <p>Польська зворотня анотація: </p>
-    <p id="pol_inv"></p>
-    <h3 id="result">Результат: <span></span></h3>
-</form>
+<p id="field"></p>
+<div id="function-btn"></div>
+<div id="custom-btn"></div>
+<div id="calculator">
+    <div id="zero">0</div>
+    <div id="dot">.</div>
+    <div id="opar">(</div>
+    <div id="cpar">)</div>
+    <div id="one">1</div>
+    <div id="two">2</div>
+    <div id="thre">3</div>
+    <div id="res">=</div>
+    <div id="four">4</div>
+    <div id="five">5</div>
+    <div id="six">6</div>
+    <div id="plus">+</div>
+    <div id="seven">7</div>
+    <div id="ege">8</div>
+    <div id="nine">9</div>
+    <div id="clear">C</div>
+    <div id="div">/</div>
+    <div id="multi">*</div>
+    <div id="minus">-</div>
+</div>
+<div id="function">
+
+</div>
+<div id="custom">
+
+    <div id="add_new_custom">Add</div>
+</div>
 </body>
 </html>
 
